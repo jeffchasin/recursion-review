83a8b28d5e441458799a0b2234b9d19446d0ef68
Jeff Chasin <jeff@jeffchasin.com>
2018-06-26 15:54:31 -0400

    fix getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,26 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  var results = [];
+  if (document.childNodes.length === 0) {
+    return results;
+  }
+  var getDom = function(topNode, results, className) {
+    var childrens = topNode.childNodes;
+    for (var i = 0; i < childrens.length; i++) {
+      if (childrens[i].nodeType === 1) {
+        var classes = childrens[i].classList;
+      }
+      if (classes && classes.contains(className)) {
+        results.push(childrens[i]);
+      }
+      if (childrens[i].childNodes[0]) {
+        getDom(childrens[i], results, className);
+      }
+    }
+  };
+  getDom(document, results, className);
+  return results;
 };
