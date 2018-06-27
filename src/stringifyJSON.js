1f219b9d37fb3df82888c3965ce71fef716c6f39
Jeff Chasin <jeff@jeffchasin.com>
2018-06-27 19:07:45 -0400

    fix stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,28 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  if (Array.isArray(obj)) {
+    var results = [];
+    obj.forEach(function(value) {
+      results.push( stringifyJSON(value) );
+    });
+    return '[' + results.join(',') + ']';
+  }
+
+  if (obj && typeof obj === 'object') {
+    var results = [];
+    for (var key in obj) {
+      if (obj[key] === undefined || typeof obj[key] === 'function') {
+        continue;
+      }
+      results.push( stringifyJSON(key) + ':' + stringifyJSON(obj[key]) );
+    }
+    return '{' + results.join(',') + '}';
+  }
+
+  return '' + obj;
 };
